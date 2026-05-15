7e451bd270e87517b870f34b73a912f8e4b1a4b1
sharon unnikrishnan <sharonunnikrishnan11@gmail.com>
2026-05-15 21:41:08 +0530

    Form requests created

---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+
+            'title' => 'required|string|max:255',
+
+            'description' => 'required',
+
+            'priority' => 'required',
+
+            'status' => 'nullable',
+
+            'due_date' => 'required|date',
+
+            'assigned_to' => 'required|exists:users,id',
+        ];
+    }
+}
